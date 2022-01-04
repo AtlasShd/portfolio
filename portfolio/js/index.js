daa92858090090f926409b20598f27a6d117d064
AtlasShd <tlsshrugged@gmail.com>
2022-01-04 19:51:49 +0300

    feat: add index.js file

---
+++ portfolio/js/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function () {
+	console.log('Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20\n\nИтого: 110 баллов');
+});
