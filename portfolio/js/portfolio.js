e1242e279484f44e37bd72d4d1462927ec781557
AtlasShd <tlsshrugged@gmail.com>
2022-01-06 21:53:45 +0300

    feat: add switch in portfolio section by season

---
+++ portfolio/js/portfolio.js
@@ -0,0 +1,39 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function () {
+	let currentTime = 0;
+
+	document.querySelectorAll('.portfolio__btn').forEach((btn, index) => {
+		btn.addEventListener('click', function (e) {
+			currentTime = index;
+			clearAllCheck();
+			e.target.classList.add('_active');
+			switchTime(btn);
+		});
+	});
+
+	function clearAllCheck() {
+		document.querySelectorAll('.portfolio__btn').forEach((item) => {
+			item.classList.remove('_active');
+		});
+	}
+
+	function switchTime(btn) {
+		document.querySelectorAll('.portfolio__img').forEach((item, index) => {
+			const time = btn.getAttribute('data-time');
+			item.setAttribute('src', `assets/img/portfolio/${time}/${index}.jpg`);
+		});
+	}
+
+	function preloadImgs() {
+		document.querySelectorAll('.portfolio__btn').forEach(btn => {
+			const season = btn.getAttribute('data-time');
+			document.querySelectorAll('.portfolio__img').forEach((img, indexImg) => {
+				const preloadImg = new Image();
+				preloadImg.setAttribute('src', `assets/img/portfolio/${season}/${indexImg}.jpg`);
+			});
+		});
+	}
+
+	preloadImgs();
+});
