5b0526eb188bd1e3c606e8a99184165f4b46c20a
AtlasShd <tlsshrugged@gmail.com>
2022-01-07 20:17:23 +0300

    feat: add localStorage for Languages and Themes

--- portfolio/js/blackAndWhite.js
+++ portfolio/js/blackAndWhite.js
@@ -1,7 +1,21 @@
 'use strict';
 
-document.querySelector('.blackAndWhite').addEventListener('click', function (e) {
-	e.target.classList.toggle('blackAndWhite_black');
+let currentTheme = 'dark';
+
+window.addEventListener('beforeunload', function () {
+	this.localStorage.setItem('theme', currentTheme);
+});
+
+window.addEventListener('load', function () {
+	if (this.localStorage.getItem('theme')) {
+		ourTheme(this.localStorage.getItem('theme'));
+	}
+});
+
+document.querySelector('.blackAndWhite').addEventListener('click', changeTheme);
+
+function changeTheme() {
+	document.querySelector('.blackAndWhite').classList.toggle('blackAndWhite_black');
 	document.querySelector('.header').classList.toggle('header__white-theme');
 	document.querySelector('.hero').classList.toggle('hero__white-theme');
 	document.querySelector('.skills').classList.toggle('skills__white-theme');
@@ -12,4 +26,15 @@ document.querySelector('.blackAndWhite').addEventListener('click', function (e) 
 	document.querySelector('.footer').classList.toggle('footer__white-theme');
 	document.querySelector('.burger').classList.toggle('burger__white-theme');
 	document.querySelector('.burgermenu').classList.toggle('burgermenu__white-theme');
-});
+	if (document.querySelector('.blackAndWhite').classList.contains('blackAndWhite_black')) {
+		currentTheme = 'white';
+	} else {
+		currentTheme = 'dark';
+	}
+}
+
+function ourTheme(theme) {
+	if (theme == 'white') {
+		changeTheme();
+	}
+}
--- portfolio/js/enru.js
+++ portfolio/js/enru.js
@@ -1,26 +1,42 @@
-'use strict'
+'use strict';
 
 import i18Obj from './translate.js';
 
-window.addEventListener('DOMContentLoaded', function () {
+let currentLang = 'en';
 
-	document.querySelectorAll('.lang__lang').forEach(lang => {
-		lang.addEventListener('click', function (e) {
-			changeLanguage(e.target.innerText);
-			changeColorLang(e.target)
-		});
-	});
-
-	function changeLanguage(lang) {
-		for (let key in i18Obj[lang]) {
-			document.querySelector(`[data-lng-${key}]`).innerText = i18Obj[lang][key];
-		}
+window.addEventListener('load', function () {
+	if (this.localStorage.getItem('lang')) {
+		currentLang = this.localStorage.getItem('lang');
+		switchLang(document.querySelector(`.lang__${currentLang}`));
 	}
-
-	function changeColorLang(selected) {
-		document.querySelectorAll('.lang__lang').forEach(lang => {
-			lang.classList.remove('lang__lang_active');
-		});
-		selected.classList.add('lang__lang_active');
-	}
+	console.log(currentLang);
 });
+
+window.addEventListener('beforeunload', function () {
+	this.localStorage.setItem('lang', currentLang);
+});
+
+document.querySelectorAll('.lang__lang').forEach(lang => {
+	lang.addEventListener('click', function (e) {
+		switchLang(e.target);
+	});
+});
+
+function switchLang(e) {
+	currentLang = e.innerText;
+	changeLanguage(currentLang);
+	changeColorLang(e);
+}
+
+function changeLanguage(lang) {
+	for (let key in i18Obj[lang]) {
+		document.querySelector(`[data-lng-${key}]`).innerText = i18Obj[lang][key];
+	}
+}
+
+function changeColorLang(selected) {
+	document.querySelectorAll('.lang__lang').forEach(lang => {
+		lang.classList.remove('lang__lang_active');
+	});
+	selected.classList.add('lang__lang_active');
+}
